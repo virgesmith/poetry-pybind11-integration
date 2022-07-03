2e23795ff21c26c9eece2d95cea2e6005699ec33
virgesmith <andrew@friarswood.net>
2022-07-03 13:02:02 +0100

    tweaks

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,22 +1,18 @@
-import poetry_pybind11_integration as pbi
+from poetry_pybind11_integration import FibGenerator, fib_recursive
 
 
-def test_version():
-    assert pbi.__version__
-
-
-def test_fib_recursive():
-    assert pbi.fib_recursive(0) == 0
-    assert pbi.fib_recursive(1) == 1
-    assert pbi.fib_recursive(2) == 1
-    assert pbi.fib_recursive(3) == 2
-    assert pbi.fib_recursive(4) == 3
+def test_fib_recursive() -> None:
+    assert fib_recursive(0) == 0
+    assert fib_recursive(1) == 1
+    assert fib_recursive(2) == 1
+    assert fib_recursive(3) == 2
+    assert fib_recursive(4) == 3
 
     for i in range(5,11):
-        assert pbi.fib_recursive(i) == pbi.fib_recursive(i-1) + pbi.fib_recursive(i-2)
+        assert fib_recursive(i) == fib_recursive(i-1) + fib_recursive(i-2)
 
-def test_fibgenerator():
-    fg = pbi.FibGenerator()
+def test_fibgenerator() -> None:
+    fg = FibGenerator()
 
     assert next(fg) == 0
     series = [next(fg) for _ in range(10)]
--- tests/test_init_subclass.py
+++ tests/test_init_subclass.py
@@ -1,23 +1,27 @@
-import poetry_pybind11_integration as ppi
+from poetry_pybind11_integration import Registry
 
-def test_registry():
+def test_registry() -> None:
 
   class PyBase:
     """Some unrelated base class"""
 
-  class Test1(ppi.Registry, PyBase, id=5):
+  class Test0(Registry, PyBase):
     pass
 
-  class Test2(ppi.Registry, PyBase, id=8):
+  class Test1(Registry, PyBase, id=5):
     pass
 
-  class Test3(PyBase, ppi.Registry, id=13, other="string"):
+  class Test2(Registry, PyBase, id=8):
     pass
 
-  #print(ppi.Registry.list)
-  assert ppi.Registry.list[Test1]["id"] == 5
-  assert ppi.Registry.list[Test2] == {"id": 8}
-  assert ppi.Registry.list[Test3] == {"id": 13, "other": "string"}
+  class Test3(PyBase, Registry, id=13, other="string"):
+    pass
+
+  assert Test0 in Registry.list
+  assert Registry.list[Test0] == {}
+  assert Registry.list[Test1]["id"] == 5
+  assert Registry.list[Test2] == {"id": 8}
+  assert Registry.list[Test3] == {"id": 13, "other": "string"}
 
 if __name__ == "__main__":
   test_registry()
